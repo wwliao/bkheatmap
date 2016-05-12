8fd4cd247fcd074eab8467d8401370fd70866213
Wen-Wei Liao <gattacaliao@gmail.com>
2016-05-12 15:24:57 -0400

    Prototype

---
+++ bkheatmap.py
@@ -0,0 +1,245 @@
+import os
+import math
+import argparse
+import pandas as pd
+import numpy as np
+import scipy.spatial.distance as dist
+import scipy.cluster.hierarchy as hier
+import matplotlib as mpl
+from matplotlib import cm
+from bokeh.plotting import figure, output_file, save, gridplot
+from bokeh.models import ColumnDataSource, HoverTool
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--palette", default="Spectral_r")
+parser.add_argument("--width", type=int, default=400)
+parser.add_argument("--height", type=int, default=400)
+parser.add_argument("table")
+args = parser.parse_args()
+
+def calc_zscore(df, scale):
+    if scale == "row":
+        df = df.T
+        df = (df - df.mean()) / df.std(ddof=0)
+        df = df.T
+    elif scale == "column":
+        df = (df - df.mean()) / df.std(ddof=0)
+    return df
+
+def cluster(df, metric="euclidean", method="single", row=True, column=True):
+    row_linkmat, col_linkmat = None, None
+    if row:
+        distmat = dist.pdist(df, metric)
+        row_linkmat = hier.linkage(distmat, method)
+        df = df.iloc[hier.leaves_list(row_linkmat), :]
+    if column:
+        df = df.T
+        distmat = dist.pdist(df, metric)
+        col_linkmat = hier.linkage(distmat, method)
+        df = df.iloc[hier.leaves_list(col_linkmat), :].T
+    return df, row_linkmat, col_linkmat
+
+def assign_color(df, value_var, colormap):
+    norm = mpl.colors.Normalize(vmin=df[value_var].min(),
+                                vmax=df[value_var].max())
+    df["color"] = df.apply(lambda s: mpl.colors.rgb2hex(
+                           cm.get_cmap(colormap)(norm(s[value_var]))),
+                           axis=1)
+    return df
+
+def assign_cat_color(df, cat_var, colormap):
+    color = {}
+    norm = mpl.colors.Normalize(vmin=0, vmax=len(df[cat_var].unique())-1)
+    for i, cat in enumerate(df[cat_var].unique()):
+        color[cat] = mpl.colors.rgb2hex(cm.get_cmap(colormap)(norm(i)))
+    df["color"] = df.apply(lambda s: color[s[cat_var]], axis=1)
+    return df
+
+def get_colorbar_source(df, value_var, colormap):
+    vmin = df[value_var].min()
+    vmax = df[value_var].max()
+    norm = mpl.colors.Normalize(vmin=vmin, vmax=vmax)
+    value = np.linspace(vmin, vmax, num=50)
+    color = []
+    for v in value:
+        color.append(mpl.colors.rgb2hex(cm.get_cmap(colormap)(norm(v))))
+    return ColumnDataSource(data=dict(value=value, color=color))
+    
+root = os.path.splitext(os.path.basename(args.table))[0]
+df = pd.read_table(args.table, index_col=0)
+df.index.name = "row"
+zscore = calc_zscore(df, scale="row")
+zscore = zscore.dropna()
+zscore, rlm, clm = cluster(zscore)
+
+coldict = dict((t[1], t[0]) for t in enumerate(zscore.columns.tolist()))
+rowdict = dict((t[1], t[0]) for t in enumerate(zscore.index.tolist()))
+tidy_df = pd.melt(zscore.reset_index(), id_vars=["row"],
+                  var_name="column", value_name="zscore")
+tidy_df["row_id"] = tidy_df.apply(lambda s: rowdict[s["row"]], axis=1)
+tidy_df["column_id"] = tidy_df.apply(lambda s: coldict[s["column"]], axis=1)
+tidy_df["value"] = tidy_df.apply(lambda s: df[s["column"]][s["row"]], axis=1)
+tidy_df = assign_color(tidy_df, "zscore", args.palette)
+
+source = ColumnDataSource(data=tidy_df)
+TOOLS = "pan,box_zoom,wheel_zoom,reset,hover"
+
+plot_hm = figure(x_range=(-0.5, max(coldict.values()) + 0.5),
+                 y_range=(-0.5, max(rowdict.values()) + 0.5),
+                 plot_width=args.width, plot_height=args.height,
+                 toolbar_location="above", tools=TOOLS, logo=None,
+                 min_border_left=0, min_border_right=0,
+                 min_border_top=0, min_border_bottom=0)
+
+plot_hm.grid.grid_line_color = None
+plot_hm.axis.visible = None
+plot_hm.outline_line_color = None
+
+plot_hm.rect("column_id", "row_id", 1, 1, source=source, color="color", line_color=None, alpha=1)
+
+plot_hm.select_one(HoverTool).tooltips = [
+    ("row", "@row"),
+    ("column", "@column"),
+    ("value", "@value"),
+    ("z-score", "@zscore")
+]
+
+row, row_id = zip(*rowdict.items())
+row_group = []
+row_name = []
+if all(s.count(":") for s in row):
+    for s in row:
+        rg, rn = s.split(":")[-2:] # only one-level grouping
+        row_group.append(rg)
+        row_name.append(rn)
+else:
+    row_name = row
+
+plot_rl = figure(plot_width=200, plot_height=plot_hm.plot_height,
+                 x_range=(0, 1), y_range=plot_hm.y_range, title=None,
+                 min_border_left=0, min_border_right=0,
+                 min_border_top=0, min_border_bottom=0,
+                 toolbar_location=None, webgl=True)
+plot_rl.text(0.05, "row_id", "row_name",
+             source=ColumnDataSource(data=dict(row_name=row_name, row_id=row_id)),
+             text_align="left", text_baseline="middle",
+             text_font_size="5pt", text_color="#a8a8a8")
+plot_rl.axis.visible = None
+plot_rl.grid.grid_line_color = None
+plot_rl.outline_line_color = None
+
+column, column_id = zip(*coldict.items())
+column_group = []
+column_name = []
+if all(s.count(":") for s in column):
+    for s in column:
+        cg, cn = s.split(":")[-2:] # only one-level grouping
+        column_group.append(cg)
+        column_name.append(cn)
+else:
+    column_name = column
+
+plot_cl = figure(plot_width=plot_hm.plot_width, plot_height=200,
+                 x_range=plot_hm.x_range, y_range=(-1, 0), title=None,
+                 min_border_left=0, min_border_right=0,
+                 min_border_top=0, min_border_bottom=0,
+                 toolbar_location=None, webgl=True)
+plot_cl.text("column_id", -0.05, "column_name",
+             source=ColumnDataSource(data=dict(column_name=column_name, column_id=column_id)),
+             text_align="right", text_baseline="middle",
+             text_font_size="5pt", text_color="#a8a8a8",
+             angle=math.pi/3)
+plot_cl.axis.visible = None
+plot_cl.grid.grid_line_color = None
+plot_cl.outline_line_color = None
+
+col_dendro = hier.dendrogram(clm, no_plot=True)
+plot_cd = figure(plot_width=plot_hm.plot_width, plot_height=180,
+                 x_range=plot_hm.x_range, title=None,
+                 min_border_left=0, min_border_right=0,
+                 min_border_top=0, min_border_bottom=0,
+                 toolbar_location=None, webgl=True)
+
+if column_group:
+    plot_cd.multi_line(list(np.asarray(col_dendro["icoord"])/10 - 0.5),
+                       list(np.asarray(col_dendro["dcoord"]) + 0.25),
+                       line_color="#a8a8a8", line_width=1)
+
+    groupdict = {}
+    groupdict["column_id"] = column_id
+    groupdict["column_group"] = column_group
+    groupdf = pd.DataFrame(groupdict)
+    groupdf = assign_cat_color(groupdf, "column_group", "Paired")
+    plot_cd.rect("column_id", 0, width=1, height=0.5,
+                 fill_color="color", line_color=None,
+                 source=ColumnDataSource(data=groupdf))
+else:
+    plot_cd.multi_line(list(np.asarray(col_dendro["icoord"])/10 - 0.5),
+                       list(np.asarray(col_dendro["dcoord"])),
+                       line_color="#a8a8a8", line_width=1)
+
+plot_cd.axis.visible = None
+plot_cd.grid.grid_line_color = None
+plot_cd.outline_line_color = None
+
+row_dendro = hier.dendrogram(rlm, orientation="left", no_plot=True)
+plot_rd = figure(plot_width=200, plot_height=plot_hm.plot_height,
+                 y_range=plot_hm.y_range, title=None,
+                 min_border_left=0, min_border_right=0,
+                 min_border_top=0, min_border_bottom=0,
+                 toolbar_location=None, webgl=True)
+
+if row_group:
+    plot_rd.multi_line(list(np.asarray(row_dendro["dcoord"])*-1 - 0.15),
+                       list(np.asarray(row_dendro["icoord"])/10 - 0.5),
+                       line_color="#a8a8a8", line_width=1)
+
+    groupdict = {}
+    groupdict["row_id"] = row_id
+    groupdict["row_group"] = row_group
+    groupdf = pd.DataFrame(groupdict)
+    groupdf = assign_cat_color(groupdf, "row_group", "Set3")
+    plot_rd.rect(0, "row_id", width=0.3, height=1,
+                 fill_color="color", line_color=None,
+                 source=ColumnDataSource(data=groupdf))
+else:
+    plot_rd.multi_line(list(np.asarray(row_dendro["dcoord"])*-1),
+                       list(np.asarray(row_dendro["icoord"])/10 - 0.5),
+                       line_color="#a8a8a8", line_width=1)
+
+plot_rd.axis.visible = None
+plot_rd.grid.grid_line_color = None
+plot_rd.outline_line_color = None
+
+plot_empty = figure(plot_width=plot_rd.plot_width,
+                    plot_height=plot_cd.plot_height, title=None,
+                    toolbar_location=None)
+plot_empty.axis.visible = None
+plot_empty.grid.grid_line_color = None
+plot_empty.outline_line_color = None
+
+plot_colorbar = figure(y_range=(-0.5, 0.5), x_axis_location="above",
+                       plot_width=plot_rd.plot_width,
+                       plot_height=plot_cd.plot_height, title=None,
+                       min_border_top=70, min_border_bottom=70,
+                       toolbar_location=None)
+plot_colorbar.rect(x="value", y=0, fill_color="color",
+                   line_color=None, width=1, height=1,
+                   source=get_colorbar_source(tidy_df, "zscore", args.palette))
+plot_colorbar.axis.axis_line_color = None
+plot_colorbar.axis.major_tick_in = 0
+plot_colorbar.xaxis.axis_label = "z-score"
+plot_colorbar.xaxis.axis_label_text_color = "#a8a8a8"
+plot_colorbar.xaxis.axis_label_text_font_size = "12pt"
+plot_colorbar.xaxis.major_label_text_color = "#a8a8a8"
+plot_colorbar.xaxis.major_tick_line_color = "#a8a8a8"
+plot_colorbar.yaxis.major_tick_line_color = None
+plot_colorbar.yaxis.major_label_text_color = None
+plot_colorbar.axis.minor_tick_line_color = None
+plot_colorbar.grid.grid_line_color = None
+plot_colorbar.outline_line_color = None
+
+output_file("{0}.bkheatmap.html".format(root), title="{0} Bokeh Heatmap".format(root))
+save(gridplot([[plot_colorbar, plot_cd, None],
+               [plot_rd, plot_hm, plot_rl],
+               [plot_empty, plot_cl, None]]))
