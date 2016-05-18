3feec9bc8e805132499ef80bfe1d554a1ff40a53
Wen-Wei Liao <gattacaliao@gmail.com>
2016-05-18 15:32:51 -0400

    Update version to 0.1.4

--- bkheatmap.py
+++ bkheatmap.py
@@ -11,7 +11,7 @@ import pandas as pd
 import scipy.spatial.distance as dist
 import scipy.cluster.hierarchy as hier
 
-__version__ = "0.1.3"
+__version__ = "0.1.4"
 
 def get_parser():
     parser = argparse.ArgumentParser()
